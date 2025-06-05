ef46c9e0b459425b0a11f6e88ef1f488f9d94436
Luis Viado <luisotavio412@gmail.com>
2025-06-05 14:55:41 -0300

    Linha do Tempo

---
+++ templatemo_578_first_portfolio/js/magnific-popup-options.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  // MagnificPopup
+  var magnifPopup = function() {
+    $('.popup-image').magnificPopup({
+      type: 'image',
+      removalDelay: 300,
+      mainClass: 'mfp-with-zoom',
+      gallery:{
+        enabled:true
+      },
+      zoom: {
+        enabled: true, // By default it's false, so don't forget to enable it
+
+        duration: 300, 
+        easing: 'ease-in-out', 
+
+        
+        opener: function(openerElement) {
+        // openerElement is the element on which popup was initialized, in this case its <a> tag
+        // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+        return openerElement.is('img') ? openerElement : openerElement.find('img');
+        }
+      }
+    });
+  };
+  
+  // Call the functions 
+  magnifPopup();
+
+});
